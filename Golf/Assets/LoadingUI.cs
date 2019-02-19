93a8cce1b16c8f36ddc8e1599e0af787f088be36
DESKTOP-EV9BEG1\Mangomilik <mangomilk114@gmail.com>
2019-02-19 23:30:16 +0900

    [배환웅] 권한 추가

--- Golf/Assets/LoadingUI.cs
+++ Golf/Assets/LoadingUI.cs
@@ -8,13 +8,47 @@ public class LoadingUI : MonoBehaviour
 {
     // TODO 로딩씬으로 변경 예정
     // Start is called before the first frame update
+
+    public bool Permissions = false;
+
     void Start()
     {
+#if (UNITY_ANDROID && !UNITY_EDITOR)
+        AndroidPermissionsManager.RequestPermission(new[] { "android.permission.READ_EXTERNAL_STORAGE" }, new AndroidPermissionCallback(
+            grantedPermission =>
+            {
+                Permissions = true;
+                // 권한이 승인 되었다.
+                //CallPermission();
+            },
+            deniedPermission =>
+            {
+                //canvas_denied.SetActive(true);
+                // 권한이 거절되었다.
+            },
+            deniedPermissionAndDontAskAgain =>
+            {
+                // 권한이 거절된데다가 다시 묻지마시오를 눌러버렸다.
+                // 안드로이드 설정창 권한에서 직접 변경 할 수 있다는 팝업을 띄우는 방식을 취해야함. 
+                //canvas_denied.SetActive(true);
+            }));
+#elif (UNITY_ANDROID && UNITY_EDITOR) || UNITY_IOS
+        Permissions = true;
+#endif
+
         StartCoroutine(LoadingData());
     }
 
     IEnumerator LoadingData()
     {
+        while(true)
+        {
+            if (Permissions)
+                break;
+
+            yield return null;
+        }
+
         yield return null;
         yield return null;
         yield return null;
