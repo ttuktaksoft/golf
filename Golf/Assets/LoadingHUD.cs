bb0fe2cfb2d3e54d05879e18000f97ca992c06db
DESKTOP-EV9BEG1\Mangomilik <mangomilk114@gmail.com>
2019-04-02 17:15:08 +0900

    [배환웅] 로딩 HUD 추가

---
+++ Golf/Assets/LoadingHUD.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingHUD : MonoBehaviour
+{
+    public Canvas HUDCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        HUDCanvas.sortingOrder = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
